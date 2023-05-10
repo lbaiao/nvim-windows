6feded4b9a1e4ae179df7249e452923a61711ee2
Lucas Pires <LucasP@leapthought.co.nz>
2023-05-10 18:05:05 -0300

    idem

---
+++ lua/configs/bufferline.lua
@@ -0,0 +1,10 @@
+-- vim.opt.termguicolors = true
+local bufferline = require('bufferline')
+bufferline.setup{
+    options = {
+        mode = "buffers",
+        -- color_icons = false,
+        show_buffer_icons = true,
+        -- show_close_icon = true,
+    }
+}
